e793fd6859ada562656c316d70d713947111db65
Pushkaran_Tyagi163 <117625210+Pushkaran163@users.noreply.github.com>
2024-11-15 18:35:38 +0530

    Add files via upload

---
+++ java.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+public class java{
+// Approach : 1 (Sorting)
+
+    static int secondLargest1(int[] arr){
+        int n = arr.length;
+        Arrays.sort(arr);
+        for(int i=n-2; i>=0; i--){
+            if(arr[i] != arr[n-1]){
+                return arr[i];
+            }
+        }
+        return -1;
+    }
+
+// Approach : 2 (Two Pass Search)
+
+static int secondLargest2(int[] arr){
+    int n = arr.length;
+    int l = -1;
+    int sl = -1;
+
+    for(int i=0; i<n-1;i++){
+        if(arr[i] > l){
+            sl = l;
+            l = arr[i];
+        }
+        else if(arr[i]<l && arr[i]>sl){
+            sl = arr[i];
+        }
+    }
+    return sl;
+
+    }
+
+}
---
+++ python.py
@@ -0,0 +1,26 @@
+# Approach : 1 (Sorting)
+def second_largest(arr):
+    arr.sort()
+    n = len(arr)
+    
+    for i in range(n-2, -1, -1):
+        if(arr[i] != arr[n-1]):
+            return arr[i]
+    
+    return -1
+
+# Approach : 2 (Two Pass Search)
+
+def second_largest(arr):
+    n = len(arr)
+    l = -1
+    sl = -1
+    
+    for i in range(n):
+        if(arr[i]> l):
+            sl = l
+            l = arr[i]
+        elif(arr[i] > sl and arr[i] < l):
+            sl = arr[i]
+    
+    return sl
